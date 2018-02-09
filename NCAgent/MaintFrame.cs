7b4dcf785b9552b8ed9884d6e870cae092ef27d5
yeongdo.jung <yeongdo.jung@hanwha.com>
2018-02-09 15:07:16 +0900

    B1

--- NCAgent/MaintFrame.cs
+++ NCAgent/MaintFrame.cs
@@ -19,7 +19,7 @@ namespace NCAgent
 
         private void sendButton_Click(object sender, EventArgs e)
         {
-            /* Send Button Click123 */
+            /* Send Button Click 1 */
         }
 
         private void connectButton_Click(object sender, EventArgs e)
